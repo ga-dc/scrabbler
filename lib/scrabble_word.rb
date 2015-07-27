88c5c56f5809c302e7bddb06a9a9d66eb3c3cf8f
Matt Ficke <matthew.ficke@gmail.com>
2015-07-27 16:34:30 -0400

    add multiplier

--- lib/scrabble_word.rb
+++ lib/scrabble_word.rb
@@ -1,5 +1,6 @@
 class ScrabbleWord
   attr_reader :word
+  #word value hash
   @@values = {
    a: 1, b: 3, c: 3, d: 2, e: 1,
    f: 4, g: 2, h: 4, i: 1, j: 8,
@@ -8,16 +9,24 @@ class ScrabbleWord
    u: 1, v: 4, w: 4, x: 8, y: 4,
    z: 10
   }
+  # create instance variable for word
   def initialize(word)
-    @word = word
+    @word = word.downcase
   end
 
   def score
-    score = 0
-    letters = @word.split("")
+    @score = 0
+    # split word into chars
+    letters = self.word.split("")
+    # add each letter's value to the total
     letters.each do |letter|
-      score = score + @@values[letter.downcase.to_sym]
+      @score = @score + @@values[letter.to_sym]
     end
-    return score
+    return @score
+  end
+  # multiply word score by multiplier
+  def multiplier_score(multiplier)
+    score_multiplied = self.score * multiplier
+    return score_multiplied
   end
 end
