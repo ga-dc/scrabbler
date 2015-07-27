678730ffefdf8e4b842275eb976a848bbd838769
KristinGreenslit <kristingreenslit@gmail.com>
2015-07-27 17:47:54 -0400

    run rspec and attempt to fix issue 1 of 5

--- lib/scrabble_word.rb
+++ lib/scrabble_word.rb
@@ -1,15 +1,25 @@
 class ScrabbleWord
   attr_reader :word
-# ^ Write a "getter" for this instance variable `@word` that returns the word as a lowercase string.
+
+  letter_values = {
+    a: 1, b: 3, c: 3, d: 2, e: 1,
+    f: 4, g: 2, h: 4, i: 1, j: 8,
+    k: 5, l: 1, m: 3, n: 1, o: 1,
+    p: 3, q: 10, r: 1, s: 1, t: 1,
+    u: 1, v: 4, w: 4, x: 8, y: 4,
+    z: 10
+  }
+
   def initialize(word)
     @word = word
   end
-end
 
   def score
-# ^ Define a method `.score` that returns the scrabble score of the word.
+    return word.score.to eq(1)
   end
 
-  def multiplier_score
-# ^ Define a method `.multiplier_score` that takes a multiplier as an argument and returns a multiplied value of the score. (Think double or triple word score.)
+  def multiplier_score(multiplier)
+    return multiplier * score
   end
+
+end
