d724f291a9fdb4cd6f0971dcf7f7e1a654c6efc3
Adrian Shepelavey <adrianshepelavey@gmail.com>
2015-07-30 22:38:45 -0400

    scrabbler code start pseudocode start

---
+++ lib/scrabble_word.rb
@@ -0,0 +1,20 @@
+class ScrabbleWord
+  def initialize(word)
+    @word = word.downcase
+	end
+
+  lettervalues = {
+  a: 1, b: 3, c: 3, d: 2, e: 1,
+  f: 4, g: 2, h: 4, i: 1, j: 8,
+  k: 5, l: 1, m: 3, n: 1, o: 1,
+  p: 3, q: 10, r: 1, s: 1, t: 1,
+  u: 1, v: 4, w: 4, x: 8, y: 4,
+  z: 10
+  }
+
+# initialize word as a variable
+# take the word in play
+# make it downcase
+# store it in the instance variable
+# parse the word for its letters
+# obtain letter values
