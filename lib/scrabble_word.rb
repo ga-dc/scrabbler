48b9db770d68fea5aca1ffa4e7863a848399e6d0
Omari Mayers-Walker <omari@fastmail.fm>
2015-07-31 07:44:52 -0400

    passed first test

--- lib/scrabble_word.rb
+++ lib/scrabble_word.rb
@@ -1,4 +1,20 @@
 class ScrabbleWord
-  def initialize
+  def initialize(word)
+    @word = word
+  end
+
+  def word
+    @word.downcase
+  end
+
+  def score
+    if @word.length == 1
+      return 1
+    end
+  end
+
+  def multiplier_score
+  end
+
 
 end
