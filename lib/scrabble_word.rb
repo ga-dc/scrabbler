6d2469573ad5827c99c7b0c33f2923adb49b2560
Bipin Neupane <icomp2@hotmail.com>
2016-03-07 18:50:20 -0500

    scrabbled

--- lib/scrabble_word.rb
+++ lib/scrabble_word.rb
@@ -1,22 +1,10 @@
 require "pry"
 
-scoreEngine = {
-  a: 1, b: 3, c: 3, d: 2, e: 1,
-  f: 4, g: 2, h: 4, i: 1, j: 8,
-  k: 5, l: 1, m: 3, n: 1, o: 1,
-  p: 3, q: 10, r: 1, s: 1, t: 1,
-  u: 1, v: 4, w: 4, x: 8, y: 4,
-  z: 10}
-
-chosenWord = []
-
-converToNum = []
-
-
-
 class  ScrabbleWord
-  def initialize(word)
+  def initialize(word, multiply)
     @word = word
+    @points = 0
+    @multiply = multiply
   end
 
 
@@ -26,11 +14,28 @@ class  ScrabbleWord
     # this turns the word into a string
   end
 
-chosenWord.push(getName)
+
+# this makes the word go into an array called chosenWord
+  def chosenWord
+    scoreEngine = {
+      "a"=>1, "b"=>3, "c"=>3, "d"=>2, "e"=>1,
+      "f"=>4, "g"=>2, "h"=>4, "i"=>1, "j"=>8,
+      "k"=>5, "l"=>1, "m"=>3, "n"=>1, "o"=>1,
+      "p"=>3, "q"=>10, "r"=>1, "s"=>1, "t"=>1,
+      "u"=>1, "v"=>4, "w"=>4, "x"=>8, "y"=>4,
+      "z"=>10 }
+    "#{@word.downcase}".split("").each do |char|
+      @points += scoreEngine[char]
+    end
+    @points * @multiply
+  end
 
 
-  def score
-    "#{@word}"
+
+
+# this converts the letters to numbers
+  def convertToNum
+
   end
 
 end
