f00fd100e327ca2dd235396a9fd0f386ee6466b4
Christine Movius <christine.movius@gmail.com>
2016-03-07 16:02:29 -0500

    Add framework for score and multiplier_score methods.

--- lib/scrabble_word.rb
+++ lib/scrabble_word.rb
@@ -1,10 +1,20 @@
 # Define a class named ScrabbleWord
 class ScrabbleWord
+  # Write a "getter" for this instance variable @word that returns the word as a lowercase string.
+  attr_accessor :word
   # Write an initialize method that takes one argument, word, and stores it in an instance variable @word.
   def initialize(word)
-    @word = word
+    @word = word.downcase
   end
 
-  # Write a "getter" for this instance variable @word that returns the word as a lowercase string.
+  # Define a method .score that returns the scrabble score of the word.
+  def score
+
+  end
+
+  # Define a method .multiplier_score that takes a multiplier as an argument and returns a multiplied value of the score. (Think double or triple word score.)
+  def multiplier_score multiplier
+    self.score * multiplier    
+  end
 
 end
