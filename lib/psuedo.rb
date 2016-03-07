586594c0233cb25cb102b6140c3d80dd467a8754
Keith Prifte <kprift@gmail.com>
2016-03-07 18:54:33 -0500

    adds everything but need to fix multiplier_score

---
+++ lib/psuedo.rb
@@ -0,0 +1,6 @@
+
+score method needs to:
+  [] loop through each letter of the word
+    [] think I need to split and store before I can loop through
+  [] find the letters matching score (from letter values table)
+  [] add all letters values together to get the score
---
+++ lib/scrabble_word.rb
@@ -0,0 +1,37 @@
+require "pry"
+
+class ScrabbleWord
+  attr_accessor :word
+
+  def initialize word
+    @word = word
+  end
+
+  def score
+    score = 0
+    letter_values = {
+      a: 1, b: 3, c: 3, d: 2, e: 1,
+      f: 4, g: 2, h: 4, i: 1, j: 8,
+      k: 5, l: 1, m: 3, n: 1, o: 1,
+      p: 3, q: 10, r: 1, s: 1, t: 1,
+      u: 1, v: 4, w: 4, x: 8, y: 4,
+      z: 10
+    }
+    split_word = @word.split("")
+
+    total_word = split_word.map do |letter|
+      letter_values[letter.to_sym]
+    end
+    score = total_word.inject(:+)
+    puts score
+  end
+
+def multiplier_score multiplier
+  this.score * multiplier
+end
+end
+
+hello = ScrabbleWord.new("hello")
+hello.score
+
+binding.pry
