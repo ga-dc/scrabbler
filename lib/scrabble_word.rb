665cf3be2760d26efd437fb899bdeb14a5d025b0
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-03-07 18:37:44 -0500

    scores now work

--- lib/scrabble_word.rb
+++ lib/scrabble_word.rb
@@ -1,20 +1,40 @@
 require "pry"
 
 class ScrabbleWord
+  attr_accessor(:word, :points, :assess, :storeWord, :pointsArr)
   def initialize(word)
     @word = word
+    @lookup = {"a"=>1, "b"=>3, "c"=>3, "d"=>2, "e"=>1,
+      "f"=>4, "g"=>2, "h"=>4, "i"=>1, "j"=>8,
+      "k"=>5, "l"=>1, "m"=>3, "n"=>1, "o"=>1,
+      "p"=>3, "q"=>10, "r"=>1, "s"=>1, "t"=>1,
+      "u"=>1, "v"=>4, "w"=>4, "x"=>8, "y"=>4,
+      "z"=>10 }
+    @points = 0
+    @storeWord = []
   end
 
   def getWord
-    "#{@word.downcase}"
+    newWord = "#{@word.downcase}"
   end
 
   def score
-    puts "working on this"
+    @storeWord = getWord.split("")
+    @storeWord.each do |letter|
+    @points += @lookup[letter]
+    end
+    @points
   end
 
 end
 
+bip = ScrabbleWord.new("Quirky")
 
 
 binding.pry
+
+
+# need a place to store the points per letter
+# split apart the scrabble word
+# assess each letter against the points per letter hash
+#  after each letter is assessed, store the cumulative value of the word somewhere
