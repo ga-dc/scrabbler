99dccb93bf990cc41490602466e19585afc042d9
Christine Movius <christine.movius@gmail.com>
2016-03-07 16:09:41 -0500

    add letters.

--- lib/scrabble_word.rb
+++ lib/scrabble_word.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 # Define a class named ScrabbleWord
 class ScrabbleWord
   # Write a "getter" for this instance variable @word that returns the word as a lowercase string.
@@ -9,12 +11,24 @@ class ScrabbleWord
 
   # Define a method .score that returns the scrabble score of the word.
   def score
-
+    #return sum of letters
   end
 
   # Define a method .multiplier_score that takes a multiplier as an argument and returns a multiplied value of the score. (Think double or triple word score.)
   def multiplier_score multiplier
-    self.score * multiplier    
+    self.score * multiplier
   end
 
 end
+
+# Here are the letter values. Think about how you might put this data in a usable format for your methods above.
+letters = {
+  a: 1, b: 3, c: 3, d: 2, e: 1,
+f: 4, g: 2, h: 4, i: 1, j: 8,
+k: 5, l: 1, m: 3, n: 1, o: 1,
+p: 3, q: 10, r: 1, s: 1, t: 1,
+u: 1, v: 4, w: 4, x: 8, y: 4,
+z: 10
+}
+
+binding.pry
