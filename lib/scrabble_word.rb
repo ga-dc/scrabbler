630aba920ae1bfdd3c13983fdeaa1bf4cdf02eda
Christine Movius <christine.movius@gmail.com>
2016-03-07 15:44:52 -0500

    Initial commit. Define class ScrabbleWord and write initialize mehod.

---
+++ lib/scrabble_word.rb
@@ -0,0 +1,10 @@
+# Define a class named ScrabbleWord
+class ScrabbleWord
+  # Write an initialize method that takes one argument, word, and stores it in an instance variable @word.
+  def initialize(word)
+    @word = word
+  end
+
+  # Write a "getter" for this instance variable @word that returns the word as a lowercase string.
+
+end
