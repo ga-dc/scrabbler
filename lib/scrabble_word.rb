e58d9f8bbd3abe9e65ccf9f68601b0756db51f51
Christine Movius <christine.movius@gmail.com>
2016-03-08 14:00:20 -0500

    convert word into array of strings. trying to convert strings into symbols.

--- lib/scrabble_word.rb
+++ lib/scrabble_word.rb
@@ -3,14 +3,33 @@ require "pry"
 # Define a class named ScrabbleWord
 class ScrabbleWord
   # Write a "getter" for this instance variable @word that returns the word as a lowercase string.
-  attr_accessor :word
+  attr_accessor :word, :letters
+
+  # make converter a class variable
+  @@converters = {
+    a: 1, b: 3, c: 3, d: 2, e: 1,
+    f: 4, g: 2, h: 4, i: 1, j: 8,
+    k: 5, l: 1, m: 3, n: 1, o: 1,
+    p: 3, q: 10, r: 1, s: 1, t: 1,
+    u: 1, v: 4, w: 4, x: 8, y: 4,
+    z: 10
+  }
+
   # Write an initialize method that takes one argument, word, and stores it in an instance variable @word.
   def initialize(word)
     @word = word.downcase
+    # split word into string of letters
+    @letters = @word.split("")
   end
 
   # Define a method .score that returns the scrabble score of the word.
   def score
+    # for each letter, convert letter to symbol
+    @letters.each do |letter|
+      @syms = letter.to_sym
+    end
+    @syms
+    # word score = 0, word score = + word score plus value of letter
     #return sum of letters
   end
 
@@ -20,15 +39,5 @@ class ScrabbleWord
   end
 
 end
-
-# Here are the letter values. Think about how you might put this data in a usable format for your methods above.
-letters = {
-  a: 1, b: 3, c: 3, d: 2, e: 1,
-f: 4, g: 2, h: 4, i: 1, j: 8,
-k: 5, l: 1, m: 3, n: 1, o: 1,
-p: 3, q: 10, r: 1, s: 1, t: 1,
-u: 1, v: 4, w: 4, x: 8, y: 4,
-z: 10
-}
 
 binding.pry
