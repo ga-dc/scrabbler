729186c6c8f4303dfa00415157ca93181521a640
Matthew Stevens <matthew.lee.stevens@gmail.com>
2016-03-09 14:06:07 -0500

    added .to_sym to fix score method added multiplier method

--- lib/scrabble_word.rb
+++ lib/scrabble_word.rb
@@ -1,5 +1,3 @@
-require "pry"
-
 class ScrabbleWord
 
   def initialize(word)
@@ -7,33 +5,24 @@ class ScrabbleWord
   end
 
   def score
-    score = 0
-    scores = {  'a' => 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1,
+    word_score = 0
+    scores = {  'a': 1, 'b': 3, 'c': 3, 'd': 2, 'e': 1,
                 'f': 4, 'g': 2, 'h': 4, 'i': 1, 'j': 8,
                 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1,
                 'p': 3, 'q': 10, 'r': 1, 's': 1, 't': 1,
                 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4,
                 'z': 10}
+    splitword = @word.downcase.split("")
 
-    splitword = @word.split("")
-    # puts splitword
     splitword.each do |letter|
-      puts ("w :#{scores['w']}")
-      puts (":w :#{scores[:w]}")
-      puts "letter: #{letter}"
-      puts ("score from letter: #{scores[letter.to_sym]}")
-      # puts scores[":#{letter}"].to_i
-      score += "#{scores[letter.to_sym]}".to_i
+      # puts ("score from letter: #{scores[letter.to_sym]}")
+      word_score += "#{scores[letter.to_sym]}".to_i
     end
+    return word_score
+  end
 
-    return score
-
+  def multiplier_score(multiplier)
+    score * multiplier
   end
 
 end
-
-tennis = ScrabbleWord.new("tennis")
-
-binding.pry
-
-puts "ayyyyyy"
