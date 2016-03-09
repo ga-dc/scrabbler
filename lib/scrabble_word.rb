ea95512389e4c75eabd06cc2c0d88eaf861ae014
Patrick Moran <patrick_moran@mac.com>
2016-03-09 10:07:49 -0500

    now using symbols in the hash

--- lib/scrabble_word.rb
+++ lib/scrabble_word.rb
@@ -11,32 +11,32 @@ class ScrabbleWord
   def initialize(word)
     @word = word
     @@letter_values = {
-      "a" => 1,
-      "b" => 3,
-      "c" => 3,
-      "d" => 2,
-      "e" => 1,
-      "f" => 4,
-      "g" => 2,
-      "h" => 4,
-      "i" => 1,
-      "j" => 8,
-      "k" => 5,
-      "l" => 1,
-      "m" => 3,
-      "n" => 1,
-      "o" => 1,
-      "p" => 3,
-      "q" => 10,
-      "r" => 1,
-      "s" => 1,
-      "t" => 1,
-      "u" => 1,
-      "v" => 4,
-      "w" => 4,
-      "x" => 8,
-      "y" => 4,
-      "z" => 10
+      a: 1,
+      b: 3,
+      c: 3,
+      d: 2,
+      e: 1,
+      f: 4,
+      g: 2,
+      h: 4,
+      i: 1,
+      j: 8,
+      k: 5,
+      l: 1,
+      m: 3,
+      n: 1,
+      o: 1,
+      p: 3,
+      q: 10,
+      r: 1,
+      s: 1,
+      t: 1,
+      u: 1,
+      v: 4,
+      w: 4,
+      x: 8,
+      y: 4,
+      z: 10
     }
     #running total
     @total = 0
@@ -52,7 +52,7 @@ class ScrabbleWord
     word.each_char do |letter|
       #find the corresponding letter in the data table / .each_pair or .fetch
       #add the vales to a running score total
-      @total += @@letter_values[letter]
+      @total += @@letter_values[letter.to_sym]
     end
     return @total
   end
