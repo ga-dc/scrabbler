eafcf3cc3b7e7d5fc9fd9032948ea3ebd1ce5627
Andrew Smith-Mui <arsm800@gmail.com>
2016-03-09 13:43:33 -0500

    created class ScrabbleWord with initialize method.

---
+++ lib/scrabble_word.rb
@@ -0,0 +1,12 @@
+require "pry"
+
+#* Define a class named ScrabbleWord with an `initialize` method that takes one argument, `word`, and stores it in an instance variable `@word`.
+
+class ScrabbleWord
+
+  def initialize(word)
+    @word = word
+  end
+end
+
+binding.pry
