24fc5bbbe87fa39a77c06c096e8eea96482a8e00
Max Kaye <maxkaye@Maxs-MacBook-Pro.local>
2016-03-09 21:44:30 -0500

    final solution

--- lib/scrabble_word.rb
+++ lib/scrabble_word.rb
@@ -1,10 +1,13 @@
 require "pry"
 
 class ScrabbleWord
-
+attr_reader :word
   def initialize(word)
-      @word = word
-      @@letter_values = {
+      @word = word.downcase
+  end
+
+  def score
+      letter_values = {
         a: 1, b: 3, c: 3, d: 2, e: 1,
         f: 4, g: 2, h: 4, i: 1, j: 8,
         k: 5, l: 1, m: 3, n: 1, o: 1,
@@ -12,29 +15,21 @@ class ScrabbleWord
         u: 1, v: 4, w: 4, x: 8, y: 4,
         z: 10
       }
-
+      score = 0
+        @word.split("").each do |letter|     #split words into letters
+        score += letter_values[letter.to_sym] # do it for each saved word
+      end
+      score
     end
 
-def word
-  @word.downcase
-end
-
-def score
-  #
-  #
-  #
-end
 
 def multiplier_score(multiplier) #score times multiplier
-     self.score #get score
-     if multiplier == 5
-       @score *= 5
-   elsif multiplier == 10
-       @score *= 10
-    end
+     self.score * multiplier
+
   end
 
 end
-
+#ruby scrabble_word.rb
+block = ScrabbleWord.new('block')
 binding.pry
 puts "abcd"
