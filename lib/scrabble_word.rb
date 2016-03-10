e529016b091ee569bb2a75a0f13e2800b59bc746
Fatai Agiri <yinka.fatai@gmail.com>
2016-03-10 02:17:58 -0500

    passed 3 rspec, need to pass multiplier

--- lib/scrabble_word.rb
+++ lib/scrabble_word.rb
@@ -1,44 +1,48 @@
 class ScrabbleWord
-  attr_accessor :word
+  attr_reader :word
 
-def initialize(word)
-  @word = word
+  def initialize(word)
+    @word = word.downcase.split("")
 
-end
+  end
 
-def score
-  @word.downcase.to_s.split("")
+  def score
+    score = 0
 
-  letter_score ={
-    a: 1,
-    b: 3,
-    c: 3,
-    d: 2,
-    e: 1,
-    f: 4,
-    g: 2,
-    h: 4,
-    i: 1,
-    j: 8,
-    k: 5,
-    l: 1,
-    m: 3,
-    n: 1,
-    o: 1,
-    p: 3,
-    q: 10,
-    r: 1,
-    s: 1,
-    t: 1,
-    u: 1,
-    v: 4,
-    w: 4,
-    x: 8,
-    y: 4,
-    z: 10
-  }
-  
-end
+    letter_score ={
+      a: 1,
+      b: 3,
+      c: 3,
+      d: 2,
+      e: 1,
+      f: 4,
+      g: 2,
+      h: 4,
+      i: 1,
+      j: 8,
+      k: 5,
+      l: 1,
+      m: 3,
+      n: 1,
+      o: 1,
+      p: 3,
+      q: 10,
+      r: 1,
+      s: 1,
+      t: 1,
+      u: 1,
+      v: 4,
+      w: 4,
+      x: 8,
+      y: 4,
+      z: 10
+    }
+    @word.each do |letter|
+      score += letter_score[letter.to_sym]
+    end
+    return score
+  end
+
 
 
 end
