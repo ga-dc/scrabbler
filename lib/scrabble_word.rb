993dbfe81add5ad7727ebf820ee2f66431120717
Lyndsey Newsome <lyndseysnewsome@gmail.com>
2016-03-09 19:44:30 -0500

    made score method.

--- lib/scrabble_word.rb
+++ lib/scrabble_word.rb
@@ -8,7 +8,7 @@ class ScrabbleWord
 
 
     def score
-      letters = @word.downcase.split("").each do |letters|
+      letters = @word.downcase.split("").each
       score = 0
       values = {a: 1, b: 3, c: 3, d: 2, e: 1,
           f: 4, g: 2, h: 4, i: 1, j: 8,
@@ -16,7 +16,7 @@ class ScrabbleWord
           p: 3, q: 10, r: 1, s: 1, t: 1,
           u: 1, v: 4, w: 4, x: 8, y: 4,
           z: 10}
-      score += score
+      score += values[letter]
     end
 end
 
