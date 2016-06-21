3f92ab793f340d48f140bf4abf8087a26943ceb6
David <dcordz@gmail.com>
2016-06-20 20:31:48 -0400

    finished but trouble with getter method downcase

---
+++ lib/scrabble_word.rb
@@ -0,0 +1,25 @@
+class ScrabbleWord
+  def initialize(word)
+    @word = word.downcase #why does this work and the below getter function not work?
+  end
+  def word
+    # @word = @word.downcase why won't this work?
+    @word
+  end
+  def score
+    letter_scores = {
+      a: 1, b: 3, c: 3, d: 2, e: 1,
+      f: 4, g: 2, h: 4, i: 1, j: 8,
+      k: 5, l: 1, m: 3, n: 1, o: 1,
+      p: 3, q: 10, r: 1, s: 1, t: 1,
+      u: 1, v: 4, w: 4, x: 8, y: 4,
+      z: 10
+    }
+    word_score = 0
+    @word.split("").each{|letter| word_score += letter_scores[letter.to_sym]}
+    return word_score
+  end
+  def multiplier_score(multiplier)
+    score*multiplier
+  end
+end
