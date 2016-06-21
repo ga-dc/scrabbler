1062c4e253bf9e0137b8429475fd743170ff4036
Michael Holzheimer <michael.r.holzheimer@gmail.com>
2016-06-21 16:34:20 -0400

    block of letters to check against forming

--- lib/scrabble_word.rb
+++ lib/scrabble_word.rb
@@ -12,10 +12,14 @@ class ScrabbleWord
 #s = score
   def s
     # for each word, split into individual letters
-    @word.split(//).each do |counter|
-      counter = 1
+    @word.split(//).each do |letters|
+      counter = 1  # replace with value of letter from array/hash
+
+      # scrabble_values = [{a: 1}, {b: 3}]
+      puts scrabble_values.find(letters)
+
       @score += counter
-      puts "hello #{@score}"
+      puts "hello #{letters} score is #{@score}"
     end
   end
 
@@ -23,17 +27,19 @@ class ScrabbleWord
   # add value to score for each letter through 'each' loop
 
 
+#attemping to figure out if letter number pairs are a has or array
 def x
   puts letters.key?(:a)
   puts letters.value?(1)
 end
 
-letters = [a: 1, b: 3, c: 3, d: 2, e: 1,
+#hash
+scrabble_values = {a: 1, b: 3, c: 3, d: 2, e: 1,
 f: 4, g: 2, h: 4, i: 1, j: 8,
 k: 5, l: 1, m: 3, n: 1, o: 1,
 p: 3, q: 10, r: 1, s: 1, t: 1,
 u: 1, v: 4, w: 4, x: 8, y: 4,
-z: 10]
+z: 10}
 
 end
 
