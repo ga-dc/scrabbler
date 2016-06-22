e235a0a31739f2220b9b680577be015fca2f2314
Thaisun Green <Tlgreen16@gmail.COM>
2016-06-22 18:54:28 -0400

    added value

--- lib/scrabble_word.rb
+++ lib/scrabble_word.rb
@@ -7,14 +7,26 @@ class ScrabbleWord
   def downcase
     @word.downcase
   end
+  lettervalue = {a: 1, b: 3, c: 3, d: 2, e: 1,
+                  f: 4, g: 2, h: 4, i: 1, j: 8,
+                  k: 5, l: 1, m: 3, n: 1, o: 1,
+                  p: 3, q: 10, r: 1, s: 1, t: 1,
+                  u: 1, v: 4, w: 4, x: 8, y: 4,
+                  z: 10}
   def score
-      @word.downcase.each_byte{|w|  w.chr}
-      # for each byte assign that a value
+      @word.downcase.each_byte do |w|
+        @value += lettervalue[w.to_sym]
+        puts @value
+      end
+      # for each byte
+      #convert to a symbol
+      # get value by hash
       # then add values together
       # show that value
   end
+
   def multiplier_score (multiplyBy)
-      score * multiplyBy
+      @value * multiplyBy
   end
 end
 
