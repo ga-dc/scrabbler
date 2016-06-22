079abfbc781a7d3544c22964daed74dbf5a92fac
Thaisun Green <Tlgreen16@gmail.COM>
2016-06-21 20:57:17 -0400

    added score and multiplier_score method

--- lib/scrabble_word.rb
+++ lib/scrabble_word.rb
@@ -1,11 +1,18 @@
+require "pry"
 class ScrabbleWord
   def initialize (word)
       @word = word
       @score = 0
   end
-  def self.downcase
+  def downcase
     @word.downcase
   end
-  
-
+  def score
+      @word.downcase.each_byte{|w|  w.chr}
+  end
+  def multiplier_score (multiplyBy)
+      score * multiplyBy
+  end
 end
+
+binding.pry
