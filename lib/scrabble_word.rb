023435228aa8dc726b3e92539d9225664e65e317
Michael L Parks <github@wmi-inc.com>
2016-06-25 17:47:00 -0400

    All Tests Passed Project Complete

--- lib/scrabble_word.rb
+++ lib/scrabble_word.rb
@@ -1,48 +1,29 @@
-require "pry"
-
+# require "pry"
 class ScrabbleWord
   attr_reader :word
+  attr_accessor :word_score
   def initialize(word)
-   @word = word.downcase!
-  end
-  def score(word)
+   @word = word.downcase
+ end ####### End initialize Definition #########
+  def score
     word_score = 0
-    # Define a method `.score` that returns the scrabble score of the word.
-    # separate word by letter
-    # compare each letter with letter value on the legend
-    # keep running total of letter scores
-    # return word score
-   return word_score
-  end
-  def multiplier_score
-    # * Define a method `.multiplier_score` that takes a multiplier as an argument
-    # and returns a multiplied value of the score. (Think double or triple word score.)
-  end
-end
-
-
-
-# # Here are the letter values. Think about how you might put this data in a
-# usable format for your methods above.
-
-# ```
-# a: 1, b: 3, c: 3, d: 2, e: 1,
-# f: 4, g: 2, h: 4, i: 1, j: 8,
-# k: 5, l: 1, m: 3, n: 1, o: 1,
-# p: 3, q: 10, r: 1, s: 1, t: 1,
-# u: 1, v: 4, w: 4, x: 8, y: 4,
-# z: 10
-# ```
-
-# my_hash = {a: 1, b:2}
-# my_letter = "b"
-# my_value = my_hash[my_letter.to_sym]
-
-## Deliverable
-
-# Submit a pull request with your current progress. Include your Completeness
-# (1-5) and Comfort (1-5). We recommend you include your pseudocode, comments,
-# and descriptions of areas where you had difficulty.
-binding.pry
-
+    letter_values = {
+                      a: 1, b: 3, c: 3, d: 2, e: 1,
+                      f: 4, g: 2, h: 4, i: 1, j: 8,
+                      k: 5, l: 1, m: 3, n: 1, o: 1,
+                      p: 3, q: 10, r: 1, s: 1, t: 1,
+                      u: 1, v: 4, w: 4, x: 8, y: 4,
+                      z: 10
+                    }
+    @word.split("").each do |letter|
+      word_score += letter_values[letter.to_sym]
+    end
+    @word_score = word_score
+  end ####### End Score Method Definition #########
+  def multiplier_score(multiplier)
+    multiplier_score = @word_score * multiplier
+    puts multiplier_score
+  end ####### End multiplier_score Method Definition #########
+end ####### End ScrabbleWord Class Definition #########
+# binding.pry
 puts "Program Execution Complete"
